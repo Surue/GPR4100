6629d76d90c502be36c949edd08157ed548063df
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-11-07 08:05:27 +0100

    Add exemple with physics

---
+++ Assets/Scripts/Objets/DestructibleObjects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class DestructibleObjects : MonoBehaviour {
+    [SerializeField] int health = 5;
+
+    void OnCollisionEnter2D(Collision2D other) {
+
+        if (other.gameObject.GetComponent<Bullet>() != null) {
+            Bullet bullet = other.gameObject.GetComponent<Bullet>();
+            health -= bullet.GetDamage();
+
+            if (health <= 0) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/Players/PlayerController.cs
+++ Assets/Scripts/Players/PlayerController.cs
@@ -10,12 +10,26 @@ public class PlayerController : MonoBehaviour {
     
     [SerializeField]
     private float speed = 4;
+    [SerializeField]
+    private float maxSpeed = 10;
 
     PlayerHealth playerHealth;
 
-    [SerializeField] bool isTop = false;
-
+   
     bool canJump = true;
+    [Header("Jump")]
+    [SerializeField] float jumpVelocity = 6;
+    [SerializeField] float raycastJumpLength = 1f;
+
+    [SerializeField] float timeStopJump = 0.1f;
+    float timerStopJump = 0;
+    [SerializeField] float jumpFallingModifier = 1;
+
+    [Header("Gun")] 
+    [SerializeField] GameObject prefabBullet;
+    [SerializeField] Transform bulletSpawnPoint;
+
+    List<Vector2> jumpPosition;
     
     // Start is called before the first frame update
     void Start() {
@@ -28,30 +42,87 @@ public class PlayerController : MonoBehaviour {
         }
 
         playerHealth = GetComponent<PlayerHealth>();
+        
+        jumpPosition = new List<Vector2>();
     }
 
     void FixedUpdate() {
         body.velocity = direction;
+
+        if (body.velocity.y < 0) {
+            body.velocity = new Vector2(body.velocity.x, body.velocity.y * jumpFallingModifier);
+        }
+
+        body.velocity = Vector2.ClampMagnitude(body.velocity, maxSpeed);
+        
+        jumpPosition.Add(transform.position);
     }
-    
-    // Update is called once per frame
-    void Update() {
-        if (isTop) {
-            direction = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
 
-            if (Input.GetAxis("Jump") > 0.1f && canJump) {
-                Debug.Log("ici");
-                direction.y += 10;
+    void CheckJump() {
+        timerStopJump -= Time.deltaTime;
+        
+        if (Input.GetAxis("Jump") > 0.1f && canJump) {
+            direction.y += jumpVelocity;
 
+            canJump = false;
+
+            timerStopJump = timeStopJump;
+        }
+        
+        //Check if grounded
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastJumpLength, LayerMask.NameToLayer("Player"));
+
+        if (timerStopJump <= 0) {
+            if (hit.rigidbody != null) {
+                canJump = true;
+            } else {
                 canJump = false;
             }
+        }
+
+//        Collider2D coll = Physics2D.OverlapBox((Vector2) transform.position + Vector2.down * 0.5f, new Vector2(1f, 0.2f), 
+//            0, LayerMask.NameToLayer("Player"));
+//
+//        if (timerStopJump <= 0) {
+//            if (coll != null) {
+//                Debug.Log(coll);
+//                canJump = true;
+//            } else {
+//                canJump = false;
+//            }
+//        }
+    }
+    
+    void Update() {
+        direction = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        CheckJump();
+
+        if (Input.GetAxis("Fire1") > 0.1f) {
+            GameObject bullet = Instantiate(prefabBullet, bulletSpawnPoint);
+            bullet.transform.parent = null;
             
-        } else {
-            direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+            bullet.GetComponent<Rigidbody2D>().velocity = Vector2.right * 10; 
         }
     }
 
-    void OnTriggerStay2D(Collider2D other) {
-        canJump = true;
+    void OnTriggerEnter2D(Collider2D other) {
+        
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine((Vector2)transform.position, (Vector2)transform.position + Vector2.down * raycastJumpLength);
+        
+        Gizmos.DrawWireCube((Vector2) transform.position + Vector2.down * 0.5f, new Vector2(1f, 0.2f));
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(bulletSpawnPoint.position, 0.3f);
+
+        if (jumpPosition != null) {
+            for (int i = 0; i < jumpPosition.Count; i++) {
+                Gizmos.DrawWireSphere(jumpPosition[i], 0.1f);
+            }
+        }
     }
 }
